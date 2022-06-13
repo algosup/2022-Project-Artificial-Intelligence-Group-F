efc076ab55b8bb9fbf73ebcdc8944fbfda4c6847
TheoDct <91249762+TheoDct@users.noreply.github.com>
2022-06-13 13:51:28 +0200

    Fix the requirements.txt and the setup.sh

--- src/setup.sh
+++ src/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-mkdir -p data/train/english mkdir data/train/french data/test/english data/test/french
+mkdir -p data/train/english data/train/french data/test/english data/test/french
+
+pip install -r requirements.txt
 
 echo "Finished to create the folders"
