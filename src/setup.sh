3fd58e1baf01a9aa71ec8b2c2fca3e8a25a29dc6
TheoDct <theo.diancourt@aglsoup.com>
2022-06-27 00:27:41 +0200

    final refactor

--- src/setup.sh
+++ src/setup.sh
@@ -2,6 +2,8 @@
 
 mkdir -p data/train/english data/train/french data/test/english data/test/french
 
+mkdir -p datasets/english datasets/french
+
 pip install -r requirements.txt
 
 echo "Finished to create the folders"
